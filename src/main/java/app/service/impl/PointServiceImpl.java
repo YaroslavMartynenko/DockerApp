2f5db37e23ac32b4795cd95da70e2c44b871996e
ymartynenko <jarlua11@gmail.com>
2020-03-11 18:14:35 +0200

    "new commit"

--- src/main/java/app/service/impl/PointServiceImpl.java
+++ src/main/java/app/service/impl/PointServiceImpl.java
@@ -3,21 +3,22 @@ package app.service.impl;
 import app.domain.Point;
 import app.repository.PointRepository;
 import app.service.PointService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@AllArgsConstructor onConstructor = @__(@Autowired))
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 @Service
 public class PointServiceImpl implements PointService {
 
     private final PointRepository pointRepository;
 
-    @Autowired
-    public PointServiceImpl(PointRepository pointRepository) {
-        this.pointRepository = pointRepository;
-    }
+//    @Autowired
+//    public PointServiceImpl(PointRepository pointRepository) {
+//        this.pointRepository = pointRepository;
+//    }
 
 //    public Point getPointById(Long id) {
 //        return pointRepository.getPointById(id);
